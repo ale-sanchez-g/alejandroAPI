9626473723a47b4041cfa4622fcefa04a891f63d
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-07-10 19:10:49 +1000

    adding giphy endpoint to pick form the top 25 search

--- app/server.js
+++ app/server.js
@@ -1,13 +1,54 @@
 var express = require('./node_modules/express');
+var request = require("request");
+const genPass = require('./passwordGenerator.js');
+const htmlPage = '<!DOCTYPE html><html><header></header><body>' +
+    ' <div style="width:300;height:300;padding-bottom:99%;position:relative;"><iframe src="' +
+    'string_to_replace' +
+    '" width="300" height="300" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="' +
+    'string_to_replace' +
+    '">via GIPHY</a></p>' +
+    '</body></html>';
+
+
 var app = express();
 var fs = require("fs");
-const genPass = require('./passwordGenerator.js');
+
+function randomIntInc (low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
 
 app.get('/', function (req, res) {
         console.log( "ALEJANDRO-API"  );
         res.end( "ALEJANDRO-API" );
 });
 
+app.get('/giphy/:tagName', function (req, res) {
+    var tagN = req.params.tagName;
+
+    var options = { method: 'GET',
+        url: 'https://api.giphy.com/v1/gifs/search',
+        qs:
+        { api_key: '114548ec7f56484e9225fa9c9d0e6f99',
+            q: tagN,
+            limit: '25',
+            offset: '0',
+            rating: 'G',
+            lang: 'en' },
+        headers:
+        { 'postman-token': '806dc5a5-431e-f3cd-9642-05a4c1085261',
+            'cache-control': 'no-cache' } };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        var dataRes = JSON.parse(body);
+        var giphyPick = randomIntInc(24,0);
+        console.log(giphyPick);
+        console.log(dataRes.data[giphyPick].embed_url);
+        var htmlResponse = htmlPage.replace("string_to_replace", dataRes.data[giphyPick].embed_url);
+        res.end(htmlResponse);
+    });
+});
 
 app.get('/:leng', function (req, res) {
 
