ca0aee5bcbe64ae9b828e936202b7c2967af4975
asanchezgiraldo <alejandro.sanchezgiraldo@vodafone.com.au>
2020-09-25 17:18:52 +1000

    install heroku npm

--- heroku.sh
+++ heroku.sh
@@ -1,5 +1,5 @@
 rm -rf node_modules
-npm install --production
+npm install heroku -g
 which heroku || wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 docker build -t registry.heroku.com/$APP_NAME/web .
 docker push registry.heroku.com/$APP_NAME/web
