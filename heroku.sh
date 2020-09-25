ce809b299fde2bced3e27ef8c98073cb463fd4ee
asanchezgiraldo <alejandro.sanchezgiraldo@vodafone.com.au>
2020-09-25 17:05:00 +1000

    fix heroku

--- heroku.sh
+++ heroku.sh
@@ -3,4 +3,4 @@ npm install --production
 which heroku || wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 docker build -t registry.heroku.com/$APP_NAME/web .
 docker push registry.heroku.com/$APP_NAME/web
-heroku container:release -a responsivefight web
+heroku container:release -a $APP_NAME web
